a99fa2a84c3de0c7ce88e5d0a61a863d9eeda742
noelli <tobiah@noell.li>
2021-07-09 15:52:38 +0200

    debugging

--- r2d2.py
+++ r2d2.py
@@ -97,6 +97,9 @@ def updateList(line):
         elif (entryName in entries):
             entries[entryName] = entryValue
             logging.info("updateList: before gyr: {}".format(line))
+            print(canvas)
+            print(bubble)
+            
             if ((canvas != "") and (bubble != "") and (entryName != 'accz')):
                 updateCanvas(canvas, bubble, waageCenterX+multiplikatorX*float(entries["accx"]), waageCenterY+multiplikatorY*float(entries["accy"]),)
                 logging.info("updateList: qyro: {}".format(line))
